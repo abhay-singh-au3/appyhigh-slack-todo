93556d3a559b6ec7c6f22168b446719edce3d0c6
Abhay <abhaysingh2402@gmail.com>
2020-04-17 15:38:25 +0530

    makei tbetter

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8000;
+const tasks = new Map();
 
 app.use(express.json());
 app.use(express.urlencoded());
@@ -10,11 +11,40 @@ app.get('/', (req, res) => {
 });
 
 app.post('/slack/command/addtask', (req, res) => {
-  const text = `Task added ${req.body.text}`;
+  const { channel_name, text } = req.body;
+  if (tasks.get(channel_name) === undefined) {
+    tasks.set(channel_name, [text]);
+  } else {
+    tasks.get(channel_name).push(text);
+  }
   res.json({
     response_type: 'ephemeral',
-    text: text,
+    text: `Task added ${text}`,
   });
+});
+
+app.post('/slack/command/listtasks', (req, res) => {
+  const { channel_name } = req.body;
+  let list = tasks.get(channel_name);
+  if (list === undefined) {
+    res.json({
+      response_type: 'ephemeral',
+      text: 'Nothing yet added to the todos of this channel',
+    });
+  } else {
+    list = list.length > 1 ? list.join('\n') : list[0];
+    res.json({
+      response_type: 'ephemeral',
+      text: list,
+    });
+  }
+});
+
+app.post('/slack/command/marktask', (req, res) => {
+  const { channel_name, text } = req.body;
+  let list = tasks.get(channel_name);
+  list.splice(list.indexOf(text), 1);
+  tasks.set(channel_name, list);
 });
 
 app.listen(PORT);
