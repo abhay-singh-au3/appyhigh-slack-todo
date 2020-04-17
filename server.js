9a38e179d7dfb6659adb7ae4e07aee71f722951e
Abhay <abhaysingh2402@gmail.com>
2020-04-17 14:38:34 +0530

    makei tbetter

--- server.js
+++ server.js
@@ -2,8 +2,19 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8000;
 
+app.use(express.json());
+app.use(express.urlencoded());
+
 app.get('/', (req, res) => {
   res.send('HELLOI');
+});
+
+app.post('/slack/command/addtast', (req, res) => {
+  const text = `Task added ${req.body.text}`;
+  res.json({
+    response_type: 'in_channel',
+    test: text,
+  });
 });
 
 app.listen(PORT);
